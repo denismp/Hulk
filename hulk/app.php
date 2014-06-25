cd92895e692128f258d8f3af5db3c4d759d96d69
Denis Putnam <denisputnam@verizon.net>
2014-06-25 16:01:27 -0400

    Created for heroku to detect the app.

---
+++ hulk/app.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<!-- Auto Generated with Sencha Architect -->
+<!-- Modifications to this file will be overwritten. -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>hulk</title>
+    <script src="http://cdn.sencha.com/ext/gpl/4.2.1/ext-all.js"></script>
+    <link rel="stylesheet" href="theme/ext-theme-classic-41df808d-6e18-45ae-a0cc-2f1c996a2db1.css">
+    <script type="text/javascript" src="app.js?_dc=1403724245021"></script>
+</head>
+<body></body>
+</html>
